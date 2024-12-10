2f09cd4774f38be8ac454312d431b22ddbd5390a
kshitiz290 <152250271+kshitiz290@users.noreply.github.com>
2024-12-10 10:48:04 +0530

    useNewUrlParser Warning removed

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect("mongodb://localhost:27017/protein", {
-        useNewUrlParser: true,
-      });
+      const conn = await mongoose.connect("mongodb://localhost:27017/protein");
       // console.log(`MongoDB Connected: {conn.connection.host}`);
     } catch (error) {
       console.error(error.message);
